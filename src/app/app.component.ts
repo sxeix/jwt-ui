b2c72f4bb50214d264c4e079e1bb3c93c3b8f8c4
sxeix <sxeixarts@gmail.com>
2023-07-24 22:11:54 +0100

    initial functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Buffer } from 'buffer';
+import * as URLSafeBase64 from 'urlsafe-base64';
+import { createHmac} from 'crypto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'jwt-ui';
+  options = ['first', 'second', 'third'];
+  optionContentMap: Map<string, string> = new Map<string, string>();
+  selected: string = '';
+  content: string | undefined = '';
+  secret = '';
+  resultToken: string = 'none'
+
+  ngOnInit() {
+    this.optionContentMap.set('first', '{"test1":"result1"}');
+    this.optionContentMap.set('second', '{"test2":"result2"}');
+    this.optionContentMap.set('third', '{"test3":"result3"}');
+  }
+
+  getContent(): string | undefined {
+    return this.optionContentMap.get(this.selected);
+  }
+
+  generate() {
+    if (this.content !== undefined) {
+      let header = '{"alg":"HS256","typ":"JWT"}';
+      let headerEncoded = URLSafeBase64.encode(Buffer.from(header));
+      let payloadEncoded = URLSafeBase64.encode(Buffer.from(this.stripSpacing(this.content)));
+      let signatureEncoded = this.getSignedStringBase64(headerEncoded, payloadEncoded);
+      this.resultToken = `${headerEncoded}.${payloadEncoded}.${signatureEncoded}`;
+    }
+  }
+
+  getSignedStringBase64(header: string, payload: string): string {
+    let hash = createHmac('sha256', this.stripSpacing(this.secret))
+            .update(`${header}.${payload}`)
+            .digest('hex');
+    return URLSafeBase64.encode(Buffer.from(hash, 'hex'));
+  }
+
+  stripSpacing(text: string): string {
+    return text.replace(/\s/g, '' );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
